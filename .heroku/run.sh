b9bed7b0a95a00759efe87cd8f4eb2f4b920e9ca
R Rishabh Keeshan <rkkeeshan989@gmail.com>
2022-11-27 17:53:50 +0530

    Update run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -5,9 +5,9 @@ wget -q http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xf ta-lib-0.4.0-src.tar.gz
 wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
 tar -xf Python-3.10.8.tgz
-mv Python-3.10.8 python
+# mv Python-3.10.8 python
 cd ta-lib
-./configure --includedir=/app/.heroku/python/include/ --libdir=/app/.heroku/python/lib --bindir=/app/.heroku/python/bin
+./configure --includedir=/app/.heroku/Python-3.10.8/include/ --libdir=/app/.heroku/Python-3.10.8/lib --bindir=/app/.heroku/Python-3.10.8/bin
 make
 make install
 cd ..
