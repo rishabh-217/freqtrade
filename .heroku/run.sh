780d02ac5c5595496d5e1eac2788d6580db442bb
R Rishabh Keeshan <rkkeeshan989@gmail.com>
2022-11-27 13:17:22 +0530

    Update run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,7 +1,11 @@
 echo 'echo "hello world"' >.heroku/run.sh
-#wget https://filebin.net/k65qt99gyzt9zlve/TA_Lib-0.4.24-cp310-cp310-win32.whl
-#pip install TA_Lib-0.4.24-cp310-cp310-win32.whl
-#./setup.sh -i
-#freqtrade create-userdir --userdir user_data
 
-
+cd .heroku
+wget -q http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xf ta-lib-0.4.0-src.tar.gz
+wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+tar -xf Python-3.5.1.tgz
+cd ta-lib
+./configure --includedir=/app/.heroku/Python-3.5.1/include/ --libdir=/app/.heroku/Python-3.5.1/lib --bindir=/app/.heroku/Python-3.5.1/bin
+make
+make install
